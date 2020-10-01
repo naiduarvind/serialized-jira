c7d1d8364696ac9f461ee6555630702c5bc2e92c
Arvind Naidu <arvind@thebility.engineer>
2020-10-01 23:39:06 +0800

    feat(serialized-jira): initial setup of lambda and api-gateway
    Signed-off-by: Arvind Naidu <arvind@thebility.engineer>

--- cdk/lib/serialized-jira-stack.ts
+++ cdk/lib/serialized-jira-stack.ts
@@ -6,6 +6,15 @@ export class SerializedJiraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // INFO: defines AWS Lambda resource for Serialized Jira
+    const serializedJira = new lambda.Function(this, 'SerializedJira', {
+      runtime: lambda.Runtime.GO_1_X, // INFO: execution environment
+      code: lambda.Code.fromAsset('../packages/serialized-jira'), // INFO: code assets from lambda directory
+      handler: 'index.handler' // INFO: file is "serialized-jira", function is "handler"
+    });
+
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: serializedJira
+    });
   }
 }
