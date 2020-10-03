5a30ff9fa56ef107f7a3dffb3450d0549a29334d
Arvind Naidu <arvind@thebility.engineer>
2020-10-03 17:00:39 +0800

    feat(serialized-jira): solved issue with cdk synth with golang lambda
    Signed-off-by: Arvind Naidu <arvind@thebility.engineer>

--- cdk/lambda/main.go
+++ cdk/lambda/main.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
 	"github.com/gin-gonic/gin"
 )
 
--- cdk/lib/serialized-jira-stack.ts
+++ cdk/lib/serialized-jira-stack.ts
@@ -1,39 +1,20 @@
-import path = require("path")
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigw from '@aws-cdk/aws-apigateway';
-import * as assets from '@aws-cdk/aws-s3-assets';
 
 export class SerializedJiraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const myLambdaAsset = new assets.Asset(
-      // @ts-ignore - this expects Construct not cdk.Construct :thinking:
-      this,
-      "HelloGoServerLambdaFnZip",
-      {
-        path: path.join(__dirname, "lambda"),
-      }
-    )
-
-    const lambdaFn = new lambda.Function(this, "HelloGoServerLambdaFn", {
-      code: lambda.Code.fromBucket(
-        myLambdaAsset.bucket,
-        myLambdaAsset.s3ObjectKey
-      ),
+    const lambdaFn = new lambda.Function(this, "SerializedJiraLambdaFn", {
+      code: lambda.Code.fromAsset('lambda'),
       runtime: lambda.Runtime.GO_1_X,
       handler: "main",
     })
 
     // API Gateway
-    new apigw.LambdaRestApi(
-      // @ts-ignore - this expects Construct not cdk.Construct :thinking:
-      this,
-      "HelloGoServerLambdaFnEndpoint",
-      {
-        handler: lambdaFn,
-      }
-    )
+    new apigw.LambdaRestApi(this, 'SerializedJiraAPIEndpoint', {
+      handler: lambdaFn
+    });
   }
 }
