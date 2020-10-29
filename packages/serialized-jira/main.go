8630417ed159e282b33e77b98f27a7ae5dbe8425
Arvind Naidu <arvind@thebility.engineer>
2020-10-29 12:45:32 +0800

    feat(serialized-jira): include build info and HTTP func for /app/debug endpoint
    Signed-off-by: Arvind Naidu <arvind@thebility.engineer>

--- packages/serialized-jira/main.go
+++ packages/serialized-jira/main.go
@@ -5,7 +5,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"os"
+	"strconv"
 	"strings"
 
 	"github.com/andygrunwald/go-jira"
@@ -15,8 +15,10 @@ import (
 )
 
 var (
-	username = os.Getenv("USERNAME")
-	password = os.Getenv("PASSWORD")
+	username  string
+	password  string
+	sha1ver   string // INFO: sha1 revision used to build the program
+	buildTime string // INFO: build timestamp of executable
 )
 
 type TicketData struct {
@@ -43,6 +45,7 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home).Methods("GET")
 	r.HandleFunc("/", send).Methods("POST")
+	r.HandleFunc("/app/debug", handleDebug).Methods("GET")
 
 	http.Handle("/", r)
 	log.Fatal(gateway.ListenAndServe(":3000", nil))
@@ -101,6 +104,40 @@ func render(w http.ResponseWriter, filename string, data interface{}) {
 		log.Println(err)
 		http.Error(w, "Sorry, something went wrong", http.StatusInternalServerError)
 	}
+}
+
+func handleDebug(w http.ResponseWriter, r *http.Request) {
+	s := fmt.Sprintf("url: %s %s", r.Method, r.RequestURI)
+	a := []string{s}
+
+	a = append(a, "Headers:")
+	for k, v := range r.Header {
+		if len(v) == 0 {
+			a = append(a, k)
+		} else if len(v) == 1 {
+			s = fmt.Sprintf("  %s: %v", k, v[0])
+			a = append(a, s)
+		} else {
+			a = append(a, "  "+k+":")
+			for _, v2 := range v {
+				a = append(a, "    "+v2)
+			}
+		}
+	}
+
+	a = append(a, "")
+	a = append(a, fmt.Sprintf("ver: https://github.com/naiduarvind/serialized-jira/commit/%s", sha1ver))
+	a = append(a, fmt.Sprintf("built on: %s", buildTime))
+
+	s = strings.Join(a, "\n")
+	servePlainText(w, s)
+}
+
+func servePlainText(w http.ResponseWriter, s string) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Content-Length", strconv.Itoa(len(s)))
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(s))
 }
 
 // TODO: Possibility of moving this block into func init()
